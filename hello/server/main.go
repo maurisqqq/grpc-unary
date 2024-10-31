a165a41efc2934f2df1e2c631016326a4b849cc6
Maulana Risqi Mustofa <maulriski01@gmail.com>
2024-10-31 08:31:32 +0700

    first commit

---
+++ hello/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "grpc-unary/hello/proto"
+
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.HelloServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("listening on %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to server: %v\n", err)
+	}
+}
